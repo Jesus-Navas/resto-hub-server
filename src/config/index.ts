bdbe18b1ed2fefaeedab08ecc777839b5b24f54d
Jesus-Navas <jesusnavas@outlook.com>
2022-06-15 21:04:39 +0200

    feat: deploy heroku

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export default {
     MONGO_DATABASE:
-        process.env.MONGO_DATABASE || 'mongodb://localhost/tailor-challenge',
+        process.env.MONGO_DATABASE || 'mongodb://localhost/resto-hub',
     PORT: process.env.PORT || 3000,
 };
